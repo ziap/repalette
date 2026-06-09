f6b9bed752e2d0c49d04d61a5fc57b7ad63bf8ea
Zap <67962871+ziap@users.noreply.github.com>
2026-06-09 17:13:45 +0700

    Port argument parsing to CLAP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::ffi::CString;
+use std::path::Path;
+
+use clap::{ArgAction, Parser};
+
+#[derive(Parser, Debug)]
+#[command(disable_help_flag = true)]
+struct Args {
+  input: Option<Box<Path>>,
+  output: Option<Box<Path>>,
+
+  #[arg(short, long, default_value="")]
+  palette: CString,
+
+  #[arg(short, long, default_value="")]
+  dither: CString,
+
+  #[arg(short = 'h', long = "help")]
+  help: bool,
+}
+
+fn help() {
+  unimplemented!("TODO: print help from C");
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args = Args::parse();
+
+  if args.help {
+    help();
+    return;
+  }
+
+  let (input, output) = match (args.input.as_deref(), args.output.as_deref()) {
+    (None, _) => {
+      eprintln!("ERROR: input file not provided");
+      std::process::exit(1);
+    }
+    (_, None) => {
+      eprintln!("ERROR: output file not provided");
+      std::process::exit(1);
+    }
+    (Some(input), Some(output)) => (input, output),
+  };
+
+
+  dbg!(args.palette);
+  dbg!(args.dither);
 }
