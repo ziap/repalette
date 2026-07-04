58a40becce3cc616641f4d31c2ba96f0a314da1f
Zap <67962871+ziap@users.noreply.github.com>
2026-07-05 01:58:29 +0700

    Centralize unsafe FFI code

---
+++ src/repalette.rs
@@ -0,0 +1,76 @@
+use std::ffi::{CStr, c_char, c_int};
+
+mod c {
+	use std::ffi::{c_char, c_int};
+	unsafe extern "C" {
+		pub fn repalette_process(
+			pixels: *mut u8,
+			width: c_int,
+			height: c_int,
+			colors: *const u32,
+			count: usize,
+			ditherer: c_int,
+		) -> c_int;
+
+		pub fn ditherer_count() -> c_int;
+		pub fn ditherer_name(index: c_int) -> *const c_char;
+		pub fn ditherer_display(index: c_int) -> *const c_char;
+	}
+}
+
+pub struct Ditherer {
+	pub name: &'static str,
+	pub display_name: &'static str,
+}
+
+fn get_str(ptr: *const c_char) -> &'static str {
+	unsafe { CStr::from_ptr(ptr).to_str().unwrap() }
+}
+
+pub fn default_ditherer() -> &'static str {
+	get_str(unsafe { c::ditherer_name(1) })
+}
+
+pub fn get_ditherers() -> Box<[Ditherer]> {
+	(0..unsafe { c::ditherer_count() })
+		.map(|i| Ditherer {
+			name: get_str(unsafe { c::ditherer_name(i) }),
+			display_name: get_str(unsafe { c::ditherer_display(i) }),
+		})
+		.collect()
+}
+
+fn dither_index(name: &str) -> c_int {
+	(0..unsafe { c::ditherer_count() })
+		.find(|&i| unsafe { CStr::from_ptr(c::ditherer_name(i)) }.to_str() == Ok(name))
+		.expect("clap validated the ditherer name")
+}
+
+pub struct ProcessingError {
+	pub status: c_int,
+}
+
+pub fn process(
+	pixels: &mut [u8],
+	width: u32,
+	height: u32,
+	colors: &[u32],
+	ditherer: &str,
+) -> Result<(), ProcessingError> {
+	unsafe {
+		let status = c::repalette_process(
+			pixels.as_mut_ptr(),
+			width as c_int,
+			height as c_int,
+			colors.as_ptr(),
+			colors.len(),
+			dither_index(ditherer),
+		);
+
+		if status == 0 {
+			Ok(())
+		} else {
+			Err(ProcessingError { status })
+		}
+	}
+}
